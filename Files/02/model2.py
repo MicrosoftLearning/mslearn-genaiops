970b0d86fe7757e7d8a7c429035005a23db0dd25
Alison Felix <felix.alison@outlook.com>
2025-05-07 19:45:22 +0100

    Update model2.py

--- Files/02/model2.py
+++ Files/02/model2.py
@@ -1,12 +1,27 @@
+import os
 import base64
+import openai
 from dotenv import load_dotenv
 from mimetypes import guess_type
 from azure.identity import DefaultAzureCredential
 from azure.ai.projects import AIProjectClient
 from azure.ai.projects.models import ConnectionType
 
-load_dotenv()
+# Function to encode a local image into a data URL
+def local_image_to_data_url(image_path):
+    # Guess the MIME type of the image based on the file extension
+    mime_type, _ = guess_type(image_path)
+    if mime_type is None:
+        mime_type = 'application/octet-stream'  # Default MIME type if none is found
 
+    # Read and encode the image file
+    with open(image_path, "rb") as image_file:
+        base64_encoded_data = base64.b64encode(image_file.read()).decode('utf-8')
+
+    # Construct the data URL
+    return f"data:{mime_type};base64,{base64_encoded_data}"
+
+load_dotenv()
 image_path = './imgs/demo.png'
 
 # Convert the local image to a data URL using the local_image_to_data_url function
@@ -28,7 +43,7 @@ default_connection = project.connections.get_default(
     connection_type=ConnectionType.AZURE_OPEN_AI,
     include_credentials=True,
 )
-chat_client = project.inference.get_chat_completions_client()
+chat_client = project.inference.get_azure_openai_client(api_version="2024-10-21")
 model_name = os.environ.get("MODEL_DEPLOYMENT2")
 
 # Define the message to send to the model
@@ -47,7 +62,7 @@ messages=[
 }}]}]
 
 # Generate a chat completion request
-response = chat_client.complete(
+response = chat_client.chat.completions.create(
   model=model_name,
   messages=messages,
   max_tokens=2000
@@ -66,7 +81,7 @@ new_prompt = "Add a legend to the plot replacing the labels"
 messages.append({"role": "user", "content": new_prompt})
 
 # Submit the new chat completion requests
-response = client.chat.completions.create(
+response = chat_client.chat.completions.create(
     model=model_name,
     messages=messages,
     max_tokens=2000 
@@ -77,17 +92,3 @@ result = response.choices[0].message.content
 # Optional - uncomment the lines below if you want to see the response to the new prompt
 #print("\nAI's response:")
 #print(response.choices[0].message.content)
-
-# Function to encode a local image into a data URL
-def local_image_to_data_url(image_path):
-    # Guess the MIME type of the image based on the file extension
-    mime_type, _ = guess_type(image_path)
-    if mime_type is None:
-        mime_type = 'application/octet-stream'  # Default MIME type if none is found
-
-    # Read and encode the image file
-    with open(image_path, "rb") as image_file:
-        base64_encoded_data = base64.b64encode(image_file.read()).decode('utf-8')
-
-    # Construct the data URL
-    return f"data:{mime_type};base64,{base64_encoded_data}"
